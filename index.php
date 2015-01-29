eb830b38f5f1abf3fd54e3a03a2abbccabc305ae
Jackson McGlasson <originalqwerty45@gmail.com>
2015-01-29 09:14:38 -0800

    ran fix php files

--- index.php
+++ index.php
@@ -11,8 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        //
-           echo "Web Server is Running Locally";
+          require_once (__DIR__ . "/controller/create-db");
         ?>
     </body>
 </html>
