6dda65ec7e11cc3260c72af59cf03e6f8527b825
Jackson McGlasson <originalqwerty45@gmail.com>
2015-01-30 09:03:46 -0800

    added database

--- controller/create-db.php
+++ controller/create-db.php
@@ -3,11 +3,22 @@
 require_once (__DIR__ . "/../model/database.php");
 
 $connection = new mysqli($host, $username, $password);
-
+//checks for connection error
 if ($connection->connect_error) {
-    die("Error: " .$connection->connect_error);
-} else {
-    echo "Success: " .$connection->host_info;
+    die("Error: " . $connection->connect_error);
 }
+
+$exists = $connection->select_db($database);
+
+if (!$exists) {
+    $query = $connection->query("CREATE DATABASE $database");
+    if ($query) {
+        echo "Succesfully created database: " . $database;
+    }
+} else {
+    echo "Database already exists";
+}
+
+$query = $connection->query("CREATE TABLE posts");
 
 $connection->close();
