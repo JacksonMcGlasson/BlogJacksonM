e2138d1ad1233464d896507ce0dace210fb7d4a2
Jackson McGlasson <originalqwerty45@gmail.com>
2015-02-05 08:52:45 -0800

    formatted and implemented open connection and close connection function

--- model/Database.php
+++ model/Database.php
@@ -1,8 +1,36 @@
 <?php
-class Database{
+
+class Database {
+
     private $connection;
     private $host;
     private $username;
     private $password;
     private $database;
+
+    public function __construct($host, $username, $password, $database) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
+    //opens connection
+    public function openConnection() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+        //checks for connection error
+        if ($this->connection->connect_error) {
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
+    }
+    //close connection
+    public function closeConnection() {
+        if(isset($this->connection)){
+            $this->connection->close();
+        }
+    }
+
+    public function query($string) {
+        
+    }
+
 }
