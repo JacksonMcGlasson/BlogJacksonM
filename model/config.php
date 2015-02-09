8f9db30ecec31f029320914c8276060409cdd02d
Jackson McGlasson <originalqwerty45@gmail.com>
2015-02-09 10:37:19 -0800

    created a registration form

--- model/config.php
+++ model/config.php
@@ -1,7 +1,8 @@
 <?php
 
-session_start();
+
 require_once (__DIR__ . "/Database.php");
+session_start();
 
 $path = "/BlogJacksonM/";
 
@@ -10,7 +11,10 @@ $username = "root";
 $password = "root";
 $database = "blog_db";
 
+
+
 if(!isset($_SESSION["connection"])) {
+    echo "connect session";
     $connection = new Database($host, $username, $password, $database);
     $_SESSION["connection"] = $connection;
 }
