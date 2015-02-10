070dd17be2125dab3280547dc409feaacc04e294
Jackson McGlasson <originalqwerty45@gmail.com>
2015-02-09 17:38:31 -0800

    built database table for users

--- controller/create-db.php
+++ controller/create-db.php
@@ -8,9 +8,23 @@ $query = $_SESSION["connection"]->query("CREATE TABLE posts("
         . "title varchar(255) NOT NULL, "
         . "post text NOT NULL,"
         . "PRIMARY KEY (id))");
-
+//checks if query was successfully created
 if ($query) {
     echo "Succesfully created table: posts";
+} else {
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
+//creates table or users
+$query = $_SESSION["connection"]->query("CREATE TABLE users("
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+        . "username varchar(30) NOT NULL, "
+        . "email varchar(50) NOT NULL,"
+        . "password char(128) NOT NULL,"
+        . "salt char(128) NOT NULL,"
+        . "PRIMARY KEY (id))");
+//checks if query was successfully created
+if ($query) {
+    echo "Succesfully created table: users";
 } else {
     echo "<p>" . $_SESSION["connection"]->error . "</p>";
 }
