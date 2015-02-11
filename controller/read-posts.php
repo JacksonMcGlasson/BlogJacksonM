00a22a3cb3b081144dfb50476a3c20e2eb17704a
Jackson McGlasson <originalqwerty45@gmail.com>
2015-02-11 09:01:52 -0800

    displayed posts from database and added new styling

---
+++ controller/read-posts.php
@@ -0,0 +1,16 @@
+<?php
+require_once (__DIR__ . "/../model/config.php");
+//retrieves post from database
+$query = "SELECT * FROM posts";
+$result = $_SESSION["connection"]->query($query);
+//shows posts
+if ($result) {
+    while($row = mysqli_fetch_array($result)){
+        echo "<div class = 'posts'>";
+        echo "<h2>" . $row['title'] . "</h2>";
+        echo "<br />";
+        echo "<p>" . $row['post'] . "</h1>";
+        echo "<br />";
+        echo "</div>";
+    }
+}
