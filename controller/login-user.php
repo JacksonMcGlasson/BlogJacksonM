29f9ba574742f2f7f30d18c32a2fe10c89078d89
Jackson McGlasson <originalqwerty45@gmail.com>
2015-02-19 08:47:29 -0800

    usernames are now case sensitive

--- controller/login-user.php
+++ controller/login-user.php
@@ -7,7 +7,7 @@ $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
 //selects the the salt and password from the users table wher the username matches the username variable
-$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE BINARY username = '$username'");
 
 //checks validility of login
 if ($query->num_rows == 1) {
