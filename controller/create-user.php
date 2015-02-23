793cf3692c42b2c3f8206b39e9f7d2b35cc516ab
Jackson McGlasson <originalqwerty45@gmail.com>
2015-02-23 10:26:40 -0800

    spacing

--- controller/create-user.php
+++ controller/create-user.php
@@ -3,20 +3,24 @@
 require_once (__DIR__ . "/../view/header.php");
 require_once (__DIR__ . "/../model/config.php");
 require_once (__DIR__ . "/../view/footer.php");
+
 //filters login data
 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
 //makes the cyrpt for the password
 $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
 
 $hashedPassword = crypt($password, $salt);
+
 //sends register info to user table
 $query = $_SESSION["connection"]->query("INSERT INTO users SET "
         . "email = '$email',"
         . "username = '$username',"
         . "password = '$hashedPassword',"
         . "salt = '$salt'");
+
 //checks if user was created successfully
 if ($query) {
     echo "Succesfully created user: $username";
--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,6 @@
 <!--link to top of page-->
     <li><a href="index.php#top">To Top of Page</a></li>
+    
 <!-- invisible paragraph for link use-->
     <p name="bottom" id="bottom">bottom</p>
   </body>
