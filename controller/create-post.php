21f16665e31af3c31154253071da0941234056a7
Jackson McGlasson <originalqwerty45@gmail.com>
2015-02-26 09:19:54 -0800

    Final commit

--- controller/create-post.php
+++ controller/create-post.php
@@ -3,7 +3,7 @@
 require_once (__DIR__ . "/../view/header.php");
 require_once (__DIR__ . "/../model/config.php");
 require_once (__DIR__ . "/../view/footer.php");
- 
+
 //filters the title and post data
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
